9e0923248bbd98792143357a61aa36b91d64dfc4
Rao-Adi <raoadnan211@gmail.com>
2026-04-17 00:11:51 +0500

    ..

--- HCMS-Api/Components/DMS/Common/Models/Document.cs
+++ HCMS-Api/Components/DMS/Common/Models/Document.cs
@@ -222,6 +222,7 @@ public class GetDocumentDto : TableFiltersDto
     public string? DocumentTypeCode { get; set; } 
 
     public string? RequestStatus { get; set; }
+    public int EmpId { get; set; }
 }
 
 
